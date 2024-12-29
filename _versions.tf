681a3a089e24b673cf88422201d3d42aac4938f2
sloniecki <sloniecki@onet.pl>
2024-12-29 16:49:12 +0100

    fix

--- _versions.tf
+++ _versions.tf
@@ -4,10 +4,7 @@ terraform {
     storage_account_name = "1096terraformbackend"                              
     container_name       = "backend"                               
     key                  = "prod.terraform.tfstate"                
-    client_id            = var.ARM_CLIENT_ID
-    client_secret        = var.ARM_CLIENT_SECRET
-    subscription_id      = var.ARM_SUBSCRIPTION_ID
-    tenant_id            = var.ARM_TENANT_ID
+
 
   }
   required_providers {
