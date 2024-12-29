416c359e42719bb6379e95e94cbe85793fe2154f
sloniecki <sloniecki@onet.pl>
2024-12-29 16:45:21 +0100

    added remote backend

---
+++ _variables.tf
@@ -0,0 +1,15 @@
+variable "ARM_CLIENT_ID" {
+  
+}
+
+variable "ARM_CLIENT_SECRET" {
+  
+}
+
+variable "ARM_TENANT_ID" {
+  
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  
+}
--- _versions.tf
+++ _versions.tf
@@ -1,4 +1,15 @@
 terraform {
+   backend "azurerm" {
+    resource_group_name  = "remoteBackend"          
+    storage_account_name = "1096terraformbackend"                              
+    container_name       = "backend"                               
+    key                  = "prod.terraform.tfstate"                
+    client_id            = var.ARM_CLIENT_ID
+    client_secret        = var.ARM_CLIENT_SECRET
+    subscription_id      = var.ARM_SUBSCRIPTION_ID
+    tenant_id            = var.ARM_TENANT_ID
+
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -11,3 +22,4 @@ provider "azurerm" {
   features {}
 
 }
+
